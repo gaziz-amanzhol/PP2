bab11f8275e1c6b89cd5de2549e8ddf18b4d1af0
Gaziz <gazizamanzol804@gmail.com>
2026-05-02 09:12:46 +0500

    added tsis3 assets

---
+++ TSIS1/config.py
@@ -0,0 +1,7 @@
+DB_CONFIG = {
+    "host": "localhost",
+    "dbname": "phonebook_db",
+    "user": "postgres",
+    "password": "gazozpipi77",
+    "port": "5432"
+}
---
+++ TSIS1/schema.sql
@@ -0,0 +1,22 @@
+        -- Contact groups
+CREATE TABLE IF NOT EXISTS groups (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL
+);
+
+        -- Osnovniye danniye
+CREATE TABLE IF NOT EXISTS contacts (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    birthday DATE,
+    group_id INTEGER REFERENCES groups(id)
+);
+
+        -- Phone numbers
+CREATE TABLE IF NOT EXISTS phones (
+    id SERIAL PRIMARY KEY,
+    contact_id INTEGER REFERENCES contacts(id) ON DELETE CASCADE,
+    phone VARCHAR(20) NOT NULL,
+    type VARCHAR(10) CHECK (type IN ('home', 'work', 'mobile'))
+);
